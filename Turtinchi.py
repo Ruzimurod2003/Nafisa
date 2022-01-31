b7c07a03693616c287e354ec368f92dd3d26791e
Ruzimurod <Ruzimurodabdunazarov@gmail.com>
2022-01-31 09:33:24 +0500

    скуфеу

--- Turtinchi.py
+++ Turtinchi.py
@@ -4,10 +4,15 @@ n=int(input("Nechta kordinata kiritasiz:"))
 maxNumber=0
 
 for i in range(1,n+1):
+    
+    ##Bu yerda x ning kordinatasi kiritiladi
     print("x[",i,"]ni kiriting:")
     x=int(input())
+    
+    ##Bu yerda y ning kordinatasi kiritiladi
     print("y[",i,"]ni kiriting:")
     y=int(input())
+    
     if ((x**2+y**2)>maxNumber):
         maxNumber=(x**2+y**2)
 
